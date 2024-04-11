c6da4e95d7f7e89f761a115c2883eb970f8e81b3
Huseyin Davutoglu <saskin@protonmail.com>
2024-04-11 14:57:13 +0200

    motorovogn

---
+++ src/main/java/com/example/motorvognregistrer1/Motorvogn.java
@@ -0,0 +1,5 @@
+package com.example.motorvognregistrer1;
+
+public class Motorvogn {
+    private String eierPerson, eiersNavn, eiersAdresse, kjennettegn, bilmerke,biltype;
+}
---
+++ src/main/java/com/example/motorvognregistrer1/MotorvognRegistrer1Application.java
@@ -0,0 +1,13 @@
+package com.example.motorvognregistrer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorvognRegistrer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorvognRegistrer1Application.class, args);
+    }
+
+}
