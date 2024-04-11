3bf89f2d131bca133197b59954c8c8b2ea7f38c9
Huseyin Davutoglu <saskin@protonmail.com>
2024-04-11 15:10:33 +0200

    123

--- src/main/java/com/example/motorvognregistrer1/Motorvogn.java
+++ src/main/java/com/example/motorvognregistrer1/Motorvogn.java
@@ -2,4 +2,61 @@ package com.example.motorvognregistrer1;
 
 public class Motorvogn {
     private String eierPerson, eiersNavn, eiersAdresse, kjennettegn, bilmerke,biltype;
+
+    public Motorvogn(String eierPerson, String eiersNavn, String eiersAdresse, String kjennettegn, String bilmerke, String biltype) {
+        this.eierPerson = eierPerson;
+        this.eiersNavn = eiersNavn;
+        this.eiersAdresse = eiersAdresse;
+        this.kjennettegn = kjennettegn;
+        this.bilmerke = bilmerke;
+        this.biltype = biltype;
+    }
+
+    public String getEierPerson() {
+        return eierPerson;
+    }
+
+    public void setEierPerson(String eierPerson) {
+        this.eierPerson = eierPerson;
+    }
+
+    public String getEiersNavn() {
+        return eiersNavn;
+    }
+
+    public void setEiersNavn(String eiersNavn) {
+        this.eiersNavn = eiersNavn;
+    }
+
+    public String getEiersAdresse() {
+        return eiersAdresse;
+    }
+
+    public void setEiersAdresse(String eiersAdresse) {
+        this.eiersAdresse = eiersAdresse;
+    }
+
+    public String getKjennettegn() {
+        return kjennettegn;
+    }
+
+    public void setKjennettegn(String kjennettegn) {
+        this.kjennettegn = kjennettegn;
+    }
+
+    public String getBilmerke() {
+        return bilmerke;
+    }
+
+    public void setBilmerke(String bilmerke) {
+        this.bilmerke = bilmerke;
+    }
+
+    public String getBiltype() {
+        return biltype;
+    }
+
+    public void setBiltype(String biltype) {
+        this.biltype = biltype;
+    }
 }
---
+++ src/main/java/com/example/motorvognregistrer1/MotorvognController.java
@@ -0,0 +1,23 @@
+package com.example.motorvognregistrer1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+@RestController
+public class MotorvognController {
+    public final ArrayList<Motorvogn> alleMotorvognRegistrer=new ArrayList<>();
+    @PostMapping("/add")
+    public void add(Motorvogn motorvogn){
+        alleMotorvognRegistrer.add(motorvogn);
+    }
+    @GetMapping("/hentalle")
+    public ArrayList<Motorvogn> hentAlleMotorvogn(){
+        return alleMotorvognRegistrer;
+    }
+    @@GetMapping("/slett")
+    public void slettAlleMotorvogn(){
+        alleMotorvognRegistrer.clear();
+    }
+}
